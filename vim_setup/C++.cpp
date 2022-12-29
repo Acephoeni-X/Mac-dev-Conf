b86e673b28cfef6c8a90eb4553d8dfb914a77b53
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2022-12-29 10:31:55 +0530

    C++ template file

---
+++ vim_setup/C++.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+// remove these lines before submitting
+using std::endl;
+std::ifstream cin("/Users/rishi/Files/C++/input.txt");
+
+// declare variable
+
+void readInput(){
+  
+}
+
+// move this line to upper
+using namespace std;
+
+void solve(){
+  
+}
+
+using namespace std;
+
+int main(){
+  readInput();
+  solve();
+  return 0;
+}
